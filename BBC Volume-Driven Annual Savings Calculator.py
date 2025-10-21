1c59aea157aca07a1de85f3d37e430659f4885a2
taylora23 <aisha.taylor@ambitionspersonnel.com>
2025-10-21 14:51:52 +0100

    Update BBC Volume-Driven Annual Savings Calculator.py

--- BBC Volume-Driven Annual Savings Calculator.py
+++ BBC Volume-Driven Annual Savings Calculator.py
@@ -1,5 +1,3 @@
-# === Streamlit Calculator ===
-
 import streamlit as st
 
 st.set_page_config(page_title="BBC Loader and HGV Driver Volume-Driven Annual Savings Calculator", layout="wide")
@@ -17,25 +15,13 @@ st.markdown(
     '<p style="font-size:30px;">Enter the quantity of loader workers:</p>',
     unsafe_allow_html=True
 )
-
 loader_qty = st.number_input("", min_value=0, step=1, format="%d", key="loader_input")
-
-st.markdown(
-    '<p style='color:red; font-size:40px; font-weight:bold; text-align:center;'>
-            Annual Savings for Loaders: {formatted_result_1}
-)
 
 st.markdown(
     '<p style="font-size:30px;">Enter the quantity of HGV driver workers:</p>',
     unsafe_allow_html=True
 )
-
 driver_qty = st.number_input("", min_value=0, step=1, format="%d", key="driver_input")
-
-st.markdown(
-    '<p style='color:red; font-size:40px; font-weight:bold; text-align:center;'>
-            Annual Savings for Drivers: {formatted_result_2}
-)
 
 if st.button("Calculate Savings"):
     result_1 = fixed_number_1 * loader_qty
@@ -43,15 +29,20 @@ if st.button("Calculate Savings"):
     total_result = result_1 + result_2
 
     formatted_result_1 = f"£{result_1:,.2f}"
-    formatted_result_2 = f"£{result_2:,.2f}
+    formatted_result_2 = f"£{result_2:,.2f}"
     formatted_total_result = f"£{total_result:,.2f}"
 
-    st.markdown(
+st.markdown(
         f"""
+        <p style='color:red; font-size:40px; font-weight:bold; text-align:center;'>
+            Annual Savings for Loaders: {formatted_result_1}
+        </p>
+        <p style='color:red; font-size:40px; font-weight:bold; text-align:center;'>
+            Annual Savings for Drivers: {formatted_result_2}
+        </p>
         <p style='color:red; font-size:40px; font-weight:bold; text-align:center;'>
             Total Annual Savings Achieved: {formatted_total_result}
         </p>
         """,
         unsafe_allow_html=True
     )
-
